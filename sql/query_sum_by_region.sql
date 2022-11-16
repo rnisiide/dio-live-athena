f9bf6d584a245f97834ba47e87e95054f23e9098
Ricardo Nisiide <103615215+rnisiide@users.noreply.github.com>
2022-11-16 20:51:56 -0300

    Update query_sum_by_region.sql

--- sql/query_sum_by_region.sql
+++ sql/query_sum_by_region.sql
@@ -3,3 +3,10 @@ select region,
 from "population"."population"
 where region='Sul'
 group by region;
+
+
+-- query para ver população de todos os estados
+select state,
+	sum(population)
+from "populationdb"."population"
+group by state;
